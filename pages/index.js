e6c51bf171a39399c0985ada067af9a6a3644c58
HamedBahram <64147793+HamedBahram@users.noreply.github.com>
2021-07-10 21:09:33 -0400

    added google domain verification meta tag

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,42 @@
+import Head from 'next/head'
 import { useSession, signIn, signOut } from 'next-auth/client'
 
 const HomePage = () => {
     const [session, loading] = useSession()
 
     return (
-        <main className='grid place-content-center place-items-center h-screen'>
-            <h1 className='text-2xl font-bold'>Welcome to NestJS</h1>
-            {session ? (
-                <>
-                    <p>Signed in as {session.user.email}</p>
-                    <button
-                        className='px-5 py-2 mt-2 bg-blue-500 rounded text-white uppercase text-sm font-medium tracking-wider focus:outline-none focus:ring focus:border-blue-300 focus:ring-offset-1'
-                        onClick={() => signOut()}
-                    >
-                        Sign Out
-                    </button>
-                </>
-            ) : (
-                <>
-                    <p>You are not signed in</p>
-                    <button
-                        className='px-5 py-2 mt-2 bg-blue-500 rounded text-white uppercase text-sm font-medium tracking-wider focus:outline-none focus:ring focus:border-blue-300 focus:ring-offset-1'
-                        onClick={() => signIn()}
-                    >
-                        Sign In
-                    </button>
-                </>
-            )}
-        </main>
+        <>
+            <Head>
+                <meta
+                    name='google-site-verification'
+                    content='0YFtempxIaGrpTuvZGjK0JrgPJsYzmZ9R6_PUxu1CV8'
+                />
+            </Head>
+            <main className='grid place-content-center place-items-center h-screen'>
+                <h1 className='text-2xl font-bold'>Welcome to NestJS</h1>
+                {session ? (
+                    <>
+                        <p>Signed in as {session.user.email}</p>
+                        <button
+                            className='px-5 py-2 mt-2 bg-blue-500 rounded text-white uppercase text-sm font-medium tracking-wider focus:outline-none focus:ring focus:border-blue-300 focus:ring-offset-1'
+                            onClick={() => signOut()}
+                        >
+                            Sign Out
+                        </button>
+                    </>
+                ) : (
+                    <>
+                        <p>You are not signed in</p>
+                        <button
+                            className='px-5 py-2 mt-2 bg-blue-500 rounded text-white uppercase text-sm font-medium tracking-wider focus:outline-none focus:ring focus:border-blue-300 focus:ring-offset-1'
+                            onClick={() => signIn()}
+                        >
+                            Sign In
+                        </button>
+                    </>
+                )}
+            </main>
+        </>
     )
 }
 
